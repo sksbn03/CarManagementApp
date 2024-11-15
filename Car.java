774e45f79574fa707e43283eb04f598512558365
hernyyan <135739853+hernyyan@users.noreply.github.com>
2024-11-14 20:31:15 -0500

    Created Car.java

---
+++ Car.java
@@ -0,0 +1,62 @@
+public class Car {
+
+	private String name;
+	private int id;
+	private boolean status;
+	private double fee;
+	
+	public Car() {
+		name = "";
+		id = 0;
+		status = false;
+		fee = 0;
+	}
+	
+	public Car(String namee, int carid, boolean s, double sf) {
+		this.name = namee;
+		this.id = carid;
+		this.status = s;
+		this.fee = sf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public double getFee() {
+		return fee;
+	}
+
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	
+	public void printme() {
+		System.out.println("----------------------------------------");
+		System.out.println("Car name: " + name);
+		System.out.println("Car ID: " + id);
+		System.out.println("Car status: " + status);
+		System.out.println("Car service fee: " + fee);
+		System.out.println("----------------------------------------");
+	}
+}
